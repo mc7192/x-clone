5ef3f61fe999acde6c4ee6816ce9ea792a622c9e
Michael Ma <michaelma@Mac.inndomain>
2025-06-05 17:18:49 -0700

    first commit, image editor in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,77 @@
-import Image from "next/image";
+"use client";
+import Feed from "./components/Feed";
+import Share from "./components/Share";
+import Link from "next/link";
+import { cn } from "./lib/utils";
+import { useState } from "react";
 
-export default function Home() {
+const imageKitAPI = process.env.NEXT_PUBLIC_URL_ENDPOINT;
+
+const Home = () => {
+  const [activeTab, setActiveTab] = useState<string | null>(null);
+  const handleOnclick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    const clickedText = e.currentTarget.textContent?.trim();
+    setActiveTab(clickedText || null);
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div className="px-4 pt-4 flex justify-evenly text-gray font-bold border-b-[1px] border-border-gray">
+        <Link
+          href="/"
+          className={cn(
+            "pb-3 flex items-center",
+            activeTab === "For you" && "border-b-4 border-icon-blue"
+          )}
+          onClick={handleOnclick}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          For you
+        </Link>
+        <Link
+          href="/"
+          className={cn(
+            "pb-3 flex items-center",
+            activeTab === "Following" && "border-b-4 border-icon-blue"
+          )}
+          onClick={handleOnclick}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Following
+        </Link>
+        <Link
+          href="/"
+          className={cn(
+            "pb-3 flex items-center",
+            activeTab === "React.js" && "border-b-4 border-icon-blue"
+          )}
+          onClick={handleOnclick}
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          React.js
+        </Link>
+        <Link
+          href="/"
+          className={cn(
+            "pb-3 flex items-center",
+            activeTab === "Javascript" && "border-b-4 border-icon-blue"
+          )}
+          onClick={handleOnclick}
+        >
+          Javascript
+        </Link>
+        <Link
+          href="/"
+          className={cn(
+            "pb-3 flex items-center",
+            activeTab === "CSS" && "border-b-4 border-icon-blue"
+          )}
+          onClick={handleOnclick}
+        >
+          CSS
+        </Link>
+      </div>
+      <Share />
+      <Feed />
     </div>
   );
-}
+};
+
+export default Home;
